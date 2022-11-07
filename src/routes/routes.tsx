595a3fda08176571c25a8ea404cf6729ea9bf154
Guilherme <guirvilela@gmail.com>
2022-11-07 11:14:47 -0300

    build

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,7 +4,7 @@ import { Home } from "../pages/Home";
 
 export const RoutesPages = () => {
   return (
-    <BrowserRouter basename="https://guirvilela.github.io/Hubert-Test/">
+    <BrowserRouter basename="/">
       <Routes>
         <Route element={<Home />} path="/" />
         <Route element={<Details />} path="/detalhes/:id" />
